2cc81849e92c0624c609aad8bd433d8e9122171a
Sue Folkerts <sfolkerts@coinstar.com>
2018-07-24 10:16:18 -0700

    before class

--- BouncingBalls.py
+++ BouncingBalls.py
@@ -1,16 +1,21 @@
-
+import sys
 import pygame
 from pygame.locals import *
 
+# NEW FOLLOWS ********************
 import random
+# NEW ABOVE **********************
 
 pygame.init()
 screen_info = pygame.display.Info()
+
 # set the width and height to the size of the screen
 size = (width, height) = (screen_info.current_w - 20,screen_info.current_h - 20)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 color = (26, 255, 255)
+
+# NEW FOLLOWS **************************************************************************************
 ball_image = pygame.image.load("ball.png")
     #pygame.image.load("C:\\Users\\Sue\\PycharmProjects\\Python2BouncingBallsPhase2\\ball.png​")
 ball_image = pygame.transform.smoothscale(ball_image, (30, 30))
@@ -21,13 +26,21 @@ speed.rotate_ip(random.randint(0, 360))
 
 def move_ball():
     ball_rect.move_ip(speed)
+# NEW ABOVE ***************************************************************************************
 
 def main():
     while True:
         clock.tick(60)
-        move_ball()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
         screen.fill(color)
+
+# NEW FOLLOWS **************************************************************************************
+        move_ball()
         screen.blit(ball_image, ball_rect)
+# NEW ABOVE ***************************************************************************************
+
         pygame.display.flip()
 
 
